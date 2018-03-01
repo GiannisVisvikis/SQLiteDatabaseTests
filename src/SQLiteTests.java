06ea9f5d48fbd95823925e30ff71fb5a80dec8a9
Giannis Visvikis <ioevisvikis@gmail.com>
2018-03-01 04:35:36 +0200

    Image files and text lines seem ok

--- src/SQLiteTests.java
+++ src/SQLiteTests.java
@@ -1,48 +1,105 @@
-import java.sql.*;
+
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SQLiteTests{
 
     public static void main(String[] args) {
 
-        try {
+        /*
+        checkEntries("/home/ioannis/Documents/quiz/cars.txt");
+        checkEntries("/home/ioannis/Documents/quiz/constructors.txt");
+        checkEntries("/home/ioannis/Documents/quiz/drivers.txt");
+        checkEntries("/home/ioannis/Documents/quiz/figures.txt");
+        checkEntries("/home/ioannis/Documents/quiz/helmets.txt");
+        checkEntries("/home/ioannis/Documents/quiz/circuits.txt");
+        */
 
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/ioannis/Documents/databases/tests.db");
 
 
-            DatabaseMetaData metadata = connection.getMetaData();
 
-            ResultSet tables = metadata.getTables("tests.db", null, "%", null);
+    }
 
-            while (tables.next()){
-                String tableName = tables.getString(3);
 
-                System.out.println(tableName);
 
-                String sqlQuery = "select name from " + tableName + " where id%2 = 0;";
 
-                Statement statement = connection.createStatement();
 
-                ResultSet results = statement.executeQuery(sqlQuery);
+    private static void checkEntries(String txtFilePath){
 
-                while (results.next()){
+        try{
 
-                    System.out.println(results.getString(1));
+            File txtFile = new File(txtFilePath);
+
+            BufferedReader reader = new BufferedReader(new FileReader(txtFile));
+
+            String line;
+            int counter = 0;
+
+            while ( (line = (reader.readLine())) != null){
+
+                counter ++;
+
+                String[] entries = line.split(",");
+
+                if(entries.length != 7)
+                    System.out.println("Error in line");
+                else {
+
+                    String imagePath = "/home/ioannis/Documents/quiz/" + entries[1] + ".png";
+
+                    File imageFile = new File(imagePath);
+
+                    if(!imageFile.getName().equalsIgnoreCase("unknown.png") && !imageFile.exists())
+                        System.out.println(imageFile.getPath() + " is not there");
 
                 }
 
 
-
-
             }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (IOException io){
+            System.out.println(io.getMessage());
+        }
+
+
+    }
+
+
+
+
+
+    private void createDatabase(String databaseName, String[] tableNames){
+
+
+           try {
+
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:/home/ioannis/Documents/quiz/databases/quiz_tests.db");
+
+
+            String sqlQuery = "insert into friends values (null, 'gamw tin', 'trela mou', 24);";
+
+            Statement statement = connection.createStatement();
+
+            statement.execute(sqlQuery);
+
 
         }
         catch (SQLException sql){
             System.out.println(sql.getMessage());
         }
 
+
+
+
     }
-
-
 
 
 
